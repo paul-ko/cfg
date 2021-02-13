e1597e0403e724fe5f96b672a29085926f58de54
Paul Koenig <pmkoenig@gmail.com>
2021-02-12 17:14:49 -0800

    Tweak git-prompt.sh

--- bash/git-prompt.sh
+++ bash/git-prompt.sh
@@ -1,6 +1,6 @@
 PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
 PS1="$PS1"'\n'                 # new line
-PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
+PS1="$PS1"'\[\033[1;35m\]'     # change to bold purple
 PS1="$PS1"'\w'                 # current working directory
 if test -z "$WINELOADERNOEXEC"
 then
@@ -17,5 +17,6 @@ then
 	fi
 fi
 PS1="$PS1"'\[\033[0m\]'        # change color
+PS1="$PS1 \t"
 PS1="$PS1"'\n'                 # new line
 PS1="$PS1"'$ '                 # prompt: always $
