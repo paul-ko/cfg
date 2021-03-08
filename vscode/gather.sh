715f6136cb1df28741399a5f5302f139e7c5291d
Paul Koenig <pmkoenig@gmail.com>
2021-03-08 07:25:25 -0800

    Implement vscode gather.sh for Linux.

--- vscode/gather.sh
+++ vscode/gather.sh
@@ -1,3 +1,8 @@
-vscode_dir=~/AppData/Roaming/Code/User/
+if test $(uname -o) == "Msys"; then
+    vscode_dir=~/AppData/Roaming/Code/User/
+else
+    vscode_dir=~/.config/Code/User/
+fi
+
 cp ${vscode_dir}/settings.json .
 cp ${vscode_dir}/keybindings.json .
