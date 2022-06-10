ffbc3ed92768235c759e1294478a5d0786376a28
Paul Koenig <pmkoenig@gmail.com>
2022-06-09 21:37:11 -0700

    Update filepath based on new user name

--- bash/wsl/music-project.sh
+++ bash/wsl/music-project.sh
@@ -3,7 +3,7 @@
 export ITUNES_XML="/mnt/e/iTunes Library.xml"
 
 music() {
-    db=/mnt/c/Users/Spence/git/itunes-to-sqlite/tunes.db
+    db=/mnt/c/Users/pmkoe/git/itunes-to-sqlite/tunes.db
     sqlite3 $db
 }
 
